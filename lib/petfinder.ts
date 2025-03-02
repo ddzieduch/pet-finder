695924c841efdf38ef4cc09158210fe55271433d
Damian Dzieduch <d.dzieduch@gmail.com>
2025-03-02 18:56:56 +0100

    Add fetchPets

---
+++ lib/petfinder.ts
@@ -0,0 +1,35 @@
+import { PetsResponse } from '../interfaces';
+
+const PETFINDER_API_URL = process.env.PETFINDER_API_URL;
+const CLIENT_ID = process.env.PETFINDER_CLIENT_ID;
+const CLIENT_SECRET = process.env.PETFINDER_CLIENT_SECRET;
+
+const fetchToken = async (): Promise<string> => {
+  const response = await fetch(`https://api.petfinder.com/v2/oauth2/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    }),
+  });
+
+  const data = await response.json();
+  return data.access_token;
+};
+
+export const fetchPets = async (): Promise<PetsResponse> => {
+  const accessToken = await fetchToken();
+
+  const response = await fetch(`${PETFINDER_API_URL}/animals`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response.json();
+};
--- pages/pets/index.tsx
+++ pages/pets/index.tsx
@@ -1,10 +1,10 @@
-import { GetStaticProps } from "next";
-import Link from "next/link";
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { fetchPets } from '../../lib/petfinder';
 
-import { Pets } from "../../interfaces";
-import { samplePetData } from "../../utils/sample-data";
-import Layout from "../../components/Layout";
-import List from "../../components/List";
+import { Pets } from '../../interfaces';
+import Layout from '../../components/Layout';
+import List from '../../components/List';
 
 type Props = {
   items: Pets[];
@@ -13,10 +13,6 @@ type Props = {
 const WithStaticProps = ({ items }: Props) => (
   <Layout title="Pets List">
     <h1>Pets List</h1>
-    <p>
-      Example fetching data from inside <code>getStaticProps()</code>.
-    </p>
-    <p>You are currently on: /pets</p>
     <List items={items} />
     <p>
       <Link href="/">Go home</Link>
@@ -28,8 +24,9 @@ export const getStaticProps: GetStaticProps = async () => {
   // Example for including static props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
-  const items: Pets[] = samplePetData;
-  return { props: { items } };
+  const pets = await fetchPets();
+
+  return { props: { items: pets.animals || [] } };
 };
 
 export default WithStaticProps;
