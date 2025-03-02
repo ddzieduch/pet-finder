4aff93c1f4b4019764a4ab4891d821e781eb7c22
Damian Dzieduch <d.dzieduch@gmail.com>
2025-03-02 15:04:29 +0100

    Style PetCard component (#1)
    * PetCard basic styling
    
    * PetCard adjustments

---
+++ components/PetCard.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import he from 'he';
+import { Pets } from '../interfaces';
+
+type Props = {
+  data: Pets;
+};
+
+const PetCard = ({ data }: Props) => {
+  return (
+    <Link
+      href="/pets/[id]"
+      as={`/pets/${data.id}`}
+      className="flex max-w-xl flex-grow flex-col items-start justify-between rounded-md ring-1 ring-slate-900/10 drop-shadow-xl"
+    >
+      <img
+        className="aspect-square w-full rounded-t-md bg-soft-silver object-cover"
+        src={data.primary_photo_cropped?.small}
+      />
+      <div className="w-full flex flex-col flex-grow p-5 cherry-blaze rounded-b-md bg-white">
+        <h3 className="text-base font-semibold text-cherry-blaze">
+          {data.type}, {data.name}
+        </h3>
+        <p className="flex-grow">{data.description && he.decode(he.decode(data.description))}</p>
+        <div className="self-start flex flex-wrap gap-6 mt-4 text-cherry-blaze">
+          <div>Age: {data.age}</div>
+          <div>Gender: {data.gender}</div>
+          <div>Size: {data.size}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default PetCard;
