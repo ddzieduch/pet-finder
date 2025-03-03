fcd379443caf08482ada8958f74d3c4bc4b717af
Damian Dzieduch <d.dzieduch@gmail.com>
2025-03-03 11:25:03 +0100

    Style PetCard

--- components/List.tsx
+++ components/List.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const List = ({ items }: Props) => (
-  <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 md:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+  <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-10 gap-y-16 pt-10 sm:mt-16 sm:pt-16 md:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
     {items.map((item) => (
       <PetCard key={item.id} data={item} />
     ))}
--- components/PetCard.tsx
+++ components/PetCard.tsx
@@ -11,13 +11,13 @@ const PetCard = ({ data }: Props) => {
     <Link
       href="/pets/[id]"
       as={`/pets/${data.id}`}
-      className="flex max-w-xl flex-grow flex-col items-start justify-between rounded-md ring-1 ring-slate-900/10 drop-shadow-xl"
+      className="flex max-w-xl flex-grow flex-col items-start justify-between rounded-2xl ring-10 ring-charcoal-house/10 drop-shadow-xl"
     >
       <img
-        className="aspect-square w-full rounded-t-md bg-soft-silver object-cover"
+        className="aspect-square w-full rounded-t-2xl bg-soft-silver object-cover"
         src={data.primary_photo_cropped?.small}
       />
-      <div className="w-full flex flex-col flex-grow p-5 cherry-blaze rounded-b-md bg-white">
+      <div className="w-full flex flex-col flex-grow p-5 cherry-blaze rounded-b-2xl bg-white">
         <h3 className="text-base font-semibold text-cherry-blaze">
           {data.type}, {data.name}
         </h3>
